cd8a2c18c87e0330939fd79a0d347cad486ad320
GawarWawar <gawarwawar@gmail.com>
2022-12-22 02:13:52 +0200

    pd.cut testing

--- database.py
+++ database.py
@@ -1,4 +1,12 @@
-#import nympy as np
+import numpy as np
 import pandas as pd
-#dt = pd.DataFrame()
-dt=pd.read_csv("123.csv")
+dickt = {
+    'row1': [1994,1995,1996,1997],
+    'row2': [2002,2003,2004,2005],
+    'row3': [2012,2014,2015,2017]
+}
+db = pd.DataFrame.from_dict(dickt)
+#print(db)
+#print(db.index)
+#print(db.columns)
+print(pd.cut(np.array([2, 7, 5, 4, 6, 3]), 2))
