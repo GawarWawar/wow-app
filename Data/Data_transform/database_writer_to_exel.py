6720fc7f9069bd918d2aba825d55949f568ba538
GawarWawar <gawarwawar@gmail.com>
2023-01-04 18:50:27 +0200

    items in exel is ready

--- Data/Data_transform/database_writer_to_exel.py
+++ Data/Data_transform/database_writer_to_exel.py
@@ -6,9 +6,40 @@ def csv_to_dataframe (csv, dataframe):
     dataframe = dataframe.transpose()
     return dataframe
 
-files = ["CSV/Items/Anub'Rekhan_10.csv"]
+files = [
+    "CSV/Items/Anub'Rekhan_10.csv",
+    "CSV/Items/Anub'Rekhan_25.csv",
+    "CSV/Items/Anub'Rekhan_10.csv",
+    "CSV/Items/Anub'Rekhan_25.csv",
+    "CSV/Items/Four_Horsemen_Chest_10.csv",
+    "CSV/Items/Four_Horsemen_Chest_25.csv",
+    "CSV/Items/Gothik_the_Harvester_10.csv",
+    "CSV/Items/Gothik_the_Harvester_25.csv",
+    "CSV/Items/Grand_Widow_Faerlina_10.csv",
+    "CSV/Items/Grand_Widow_Faerlina_25.csv",
+    "CSV/Items/Grobbulus_10.csv",
+    "CSV/Items/Grobbulus_25.csv",
+    "CSV/Items/Heigan_the_Unclean_10.csv",
+    "CSV/Items/Heigan_the_Unclean_25.csv",
+    "CSV/Items/Instructor_Razuvious_10.csv",
+    "CSV/Items/Instructor_Razuvious_25.csv",
+    "CSV/Items/Kel'Thuzad_10.csv",
+    "CSV/Items/Kel'Thuzad_25.csv",
+    "CSV/Items/Loatheb_10.csv",
+    "CSV/Items/Loatheb_25.csv",
+    "CSV/Items/Maexxna_10.csv",
+    "CSV/Items/Maexxna_25.csv",
+    "CSV/Items/Naxxramas_trash_10.csv",
+    "CSV/Items/Noth_the_Plaguebringer_10.csv",
+    "CSV/Items/Noth_the_Plaguebringer_25.csv",
+    "CSV/Items/Patchwerk_10.csv",
+    "CSV/Items/Patchwerk_25.csv",
+    "CSV/Items/Sapphiron_10.csv",
+    "CSV/Items/Sapphiron_25.csv",
+    "CSV/Items/Thaddius_10.csv",
+    "CSV/Items/Thaddius_25.csv"
+    ]
 main_df = pd.DataFrame()
-i = 0
 
 for i in files:
     df_situational = pd.DataFrame()
