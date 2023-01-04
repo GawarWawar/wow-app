4f829201319cbc4ff8b7e868b18d0d5e26e427b0
GawarWawar <gawarwawar@gmail.com>
2023-01-04 18:12:19 +0200

    reform dbase writer mainly

--- Data/Data_transform/database_writer_to_exel.py
+++ Data/Data_transform/database_writer_to_exel.py
@@ -1,10 +1,20 @@
 import numpy as np
 import pandas as pd
 
-csv = "CSV/Items_from_Anub'Rekhan_10.csv"
-df = pd.read_csv(csv, engine='python', sep = ", ", index_col = False, header=None)
-df = df.transpose()
+def csv_to_dataframe (csv, dataframe):
+    dataframe = pd.read_csv(csv, engine='python', sep = ", ", index_col = False, header=None)
+    dataframe = dataframe.transpose()
+    return dataframe
+
+files = ["CSV/Items/Anub'Rekhan_10.csv"]
+main_df = pd.DataFrame()
+i = 0
+
+for i in files:
+    df_situational = pd.DataFrame()
+    df_situational = csv_to_dataframe (i, df_situational)
+    main_df = pd.concat([main_df,df_situational])
 
 excel = "../Items.xlsx"
-df.to_excel(excel, index=False)  
-print(df)
+main_df.to_excel(excel, index=False)  
+print(main_df)
