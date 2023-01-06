0ca4267e60ecc919224dad6a1d38e02aeab7be8e
GawarWawar <gawarwawar@gmail.com>
2023-01-06 14:35:38 +0200

    minor adjastments

--- Data/Data_transform/df_csv_to_exel.py
+++ Data/Data_transform/df_csv_to_exel.py
@@ -4,8 +4,13 @@ import time
 
 start_timer = time.perf_counter()
 
-def csv_with_no_header_to_transposed_dataframe (csv, dataframe, set_names_for_transposed_indexes=False, separetor = False):
-    #transform from csv into DataFrame with forward transposing it`s content
+#transform from csv into DataFrame with forward transposing it`s content
+def csv_with_no_header_to_transposed_dataframe (
+    csv, #file to read from
+    dataframe, #dataframe to write into
+    set_names_for_transposed_indexes=False, #names for the indexes (optional)
+    separetor = False #separator into csv file (optional)
+):
     if separetor != False: 
         dataframe = pd.read_csv(csv, engine='python', sep = separetor, header=None)
     else:
@@ -22,26 +27,26 @@ def from_many_csv_to_one_fiel_of_any_filetype (
     set_index_names, #names for columns/indexes for ur dataframe
     csv_separator=False #separator for csv files (oprional)
 ):
-    main_df = pd.DataFrame(columns=set_index_names)
     #dataframe to combine all of the file content in
+    main_df = pd.DataFrame(columns=set_index_names)
     
-    for i in files_to_read:
-        #cycle to get every csv-file into our main DataFrame 
-        df_situational = pd.DataFrame()
+    #cycle to get every csv-file into our main DataFrame
+    for i in files_to_read: 
         #dataframe for each step of the cycle
+        df_situational = pd.DataFrame()
+        #reading next file our situational dataframe (df_situational) and transposing it
         df_situational = csv_with_no_header_to_transposed_dataframe(
             csv = i,
             dataframe = df_situational, 
             set_names_for_transposed_indexes=[set_index_names], 
             separetor=csv_separator
         )
-        #reading next file our situational dataframe (df_situational) and transposing it
-        main_df = pd.concat([main_df,df_situational], ignore_index=True)
         #writing transposed situational dataframe (df_situational) into our main storage container
+        main_df = pd.concat([main_df,df_situational], ignore_index=True)
     
+    #deliting dublicates and sorting by the first element of set_index_names
     main_df = main_df.drop_duplicates(set_index_names[0])
     main_df = main_df.sort_values(set_index_names[0], ignore_index=True)
-    #deliting dublocates and sorting by the first (0) element of set_index_names
     
     #writing into specified file_type file
     if ftw_type == "csv":
