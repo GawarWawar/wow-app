9c6eeabcdfc2bce98aef6b56a47c785fe0c888df
GawarWawar <gawarwawar@gmail.com>
2023-01-06 16:09:29 +0200

    create file for Gluth

--- Data/Data_transform/from_many_csv_to_1_file_csv-json-exel.py
+++ Data/Data_transform/from_many_csv_to_1_file_csv-json-exel.py
@@ -9,7 +9,7 @@ def csv_with_no_header_to_transposed_dataframe (
     csv, #file to read from
     dataframe, #dataframe to write into
     set_names_for_transposed_indexes=False, #names for the indexes (optional)
-    separetor = False #separator into csv file (optional)
+    separetor = False, #separator into csv file (optional)
 ):
     if separetor != False: 
         dataframe = pd.read_csv(csv, engine='python', sep = separetor, header=None)
@@ -26,7 +26,8 @@ def from_many_csv_to_one_file_of_any_filetype (
     file_to_write, #file to write into
     ftw_type,      #type of file to write into, can support: json, csv or exel (xlsx) 
     set_index_names, #names for columns/indexes for ur dataframe
-    csv_separator=False #separator for csv files (oprional)
+    csv_separator=False, #separator for csv files (oprional)
+    should_be_df_transposed = True, #should dataframe be transposed, boolean, True by default (optional)
 ):
     #dataframe to combine all of the file content in
     main_df = pd.DataFrame(columns=set_index_names)
@@ -44,11 +45,15 @@ def from_many_csv_to_one_file_of_any_filetype (
         )
         #writing transposed situational dataframe (df_situational) into our main storage container
         main_df = pd.concat([main_df,df_situational], ignore_index=True)
+     
     
     #deliting dublicates and sorting by the first element of set_index_names
     main_df = main_df.drop_duplicates(set_index_names[0])
     main_df = main_df.sort_values(set_index_names[0], ignore_index=True)
     
+    if should_be_df_transposed != True:
+        main_df = main_df.transpose()
+
     #writing into specified file_type file
     if ftw_type == "csv":
         main_df.to_csv(file_to_write, index_label=False, header=True, index=False)
@@ -95,19 +100,20 @@ files = [
 ]
 
 wowhead_separator = ", "
+indexes_that_we_want_to_set_up = ["Item_id", "Item_Name"]
 
 from_many_csv_to_one_file_of_any_filetype(
     files_to_read=files,
     file_to_write="Data/Items.csv",
     ftw_type = "csv",
-    set_index_names=["Item_id", "Item_Name"],
+    set_index_names=indexes_that_we_want_to_set_up,
     csv_separator = wowhead_separator
 )
 from_many_csv_to_one_file_of_any_filetype(
     files_to_read=files,
     file_to_write="Data/Items.json",
     ftw_type = "json",
-    set_index_names=["Item_id", "Item_Name"],
+    set_index_names=indexes_that_we_want_to_set_up,
     csv_separator = wowhead_separator
 )
 
@@ -115,8 +121,55 @@ from_many_csv_to_one_file_of_any_filetype(
     files_to_read=files,
     file_to_write="Data/Items.xlsx",
     ftw_type = "exel",
-    set_index_names=["Item_id", "Item_Name"],
+    set_index_names=indexes_that_we_want_to_set_up,
     csv_separator = wowhead_separator
+)
+
+#creating Gluth_10 file
+files_for_gluth_10 = [
+    "Data/Data_transform/CSV/Items/Anub'Rekhan_10.csv",
+    "Data/Data_transform/CSV/Items/Four_Horsemen_Chest_10.csv",
+    "Data/Data_transform/CSV/Items/Gothik_the_Harvester_10.csv",
+    "Data/Data_transform/CSV/Items/Grand_Widow_Faerlina_10.csv",
+    "Data/Data_transform/CSV/Items/Grobbulus_10.csv",
+    "Data/Data_transform/CSV/Items/Heigan_the_Unclean_10.csv",
+    "Data/Data_transform/CSV/Items/Instructor_Razuvious_10.csv",
+    "Data/Data_transform/CSV/Items/Loatheb_10.csv",
+    "Data/Data_transform/CSV/Items/Maexxna_10.csv",
+    "Data/Data_transform/CSV/Items/Patchwerk_10.csv",
+    "Data/Data_transform/CSV/Items/Noth_the_Plaguebringer_10.csv",
+    "Data/Data_transform/CSV/Items/Thaddius_10.csv"
+]
+from_many_csv_to_one_file_of_any_filetype(
+    files_to_read = files_for_gluth_10,
+    file_to_write = "Data/Data_transform/CSV/Items/Gluth_10_not_wowhead.csv",
+    ftw_type = "csv",
+    set_index_names=indexes_that_we_want_to_set_up,
+    csv_separator=wowhead_separator,
+    should_be_df_transposed=False  
+)
+#creating Gluth_25 file
+files_for_gluth_25 = [
+    "Data/Data_transform/CSV/Items/Anub'Rekhan_25.csv",
+    "Data/Data_transform/CSV/Items/Four_Horsemen_Chest_25.csv",
+    "Data/Data_transform/CSV/Items/Gothik_the_Harvester_25.csv",
+    "Data/Data_transform/CSV/Items/Grand_Widow_Faerlina_25.csv",
+    "Data/Data_transform/CSV/Items/Grobbulus_25.csv",
+    "Data/Data_transform/CSV/Items/Heigan_the_Unclean_25.csv",
+    "Data/Data_transform/CSV/Items/Instructor_Razuvious_25.csv",
+    "Data/Data_transform/CSV/Items/Loatheb_25.csv",
+    "Data/Data_transform/CSV/Items/Maexxna_25.csv",
+    "Data/Data_transform/CSV/Items/Patchwerk_25.csv",
+    "Data/Data_transform/CSV/Items/Noth_the_Plaguebringer_25.csv",
+    "Data/Data_transform/CSV/Items/Thaddius_25.csv"
+]
+from_many_csv_to_one_file_of_any_filetype(
+    files_to_read = files_for_gluth_25,
+    file_to_write = "Data/Data_transform/CSV/Items/Gluth_25_not_wowhead.csv",
+    ftw_type = "csv",
+    set_index_names=indexes_that_we_want_to_set_up,
+    csv_separator=wowhead_separator,
+    should_be_df_transposed=False  
 )
 
 end_timer = time.perf_counter()
