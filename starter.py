26cde40c0959e40b9b932bb190c84131bd8ab757
GawarWawar <gawarwawar@gmail.com>
2023-01-08 13:44:52 +0200

    flask start

---
+++ starter.py
@@ -0,0 +1,24 @@
+from flask import Flask
+from flask import request
+from markupsafe import escape
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Index Page'
+
+@app.route('/hello')
+def hello():
+    return 'Hello, World'
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if valid_login(request.form['username'],
+                       request.form['password']):
+            return log_the_user_in(request.form['username'])
+        else:
+            error = 'Invalid username/password'
+    
