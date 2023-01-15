9e04db1d0991b5b33290314faa9438e647a88aee
GawarWawar <gawarwawar@gmail.com>
2023-01-15 18:00:11 +0200

    find many in df is ready

--- utils/tools.py
+++ utils/tools.py
@@ -15,7 +15,7 @@ def read_columns_of_the_csv_to_DF (
     df = pd.read_csv(file_to_read, header = 0, usecols=columns)
     #end timer
     end_timer = time.perf_counter()
-    print("read_columns_of_the_csv_to_DF timer =",end_timer-start_timer)
+    print(read_columns_of_the_csv_to_DF.__name__, " timer =",end_timer-start_timer)
     print(df.columns)
     return(df)
 
@@ -37,7 +37,7 @@ def read_the_file_to_DF (
         return(print ("Error, we dont suppot such file extention"))
     #end timer
     end_timer = time.perf_counter()
-    print("read_the_file_to_DF timer =",end_timer-start_timer)
+    print(read_the_file_to_DF.__name__, " timer =",end_timer-start_timer)
     return(df)
 
 def find_1_row_in_DataFrame (
@@ -50,13 +50,14 @@ def find_1_row_in_DataFrame (
     """
     #start timer
     start_timer = time.perf_counter()
-    #looking for the item
+    
+    #loop to look for the item
     j = 0
     for i in main_df.loc[:,item_column]:
         if i == object_to_search_for:
             #end_timer
             end_timer = time.perf_counter()
-            print("find_item_in_DataFrame timer =", end_timer-start_timer)
+            print(find_1_row_in_DataFrame.__name__," timer =", end_timer-start_timer)
             return(main_df.iloc[j])
         j = j+1
 
@@ -70,47 +71,24 @@ def find_many_rows_in_DataFrame (
     """
     #start timer
     start_timer = time.perf_counter()
-    #looking for the item
-    df_to_return = pd.DataFrame(columns=main_df.columns)
-    j = 0
-    for i in main_df.loc[:,item_column]:
-        if i == object_to_search_for:
-            #df_to_return=main_df.iloc[j]
-            #df_to_return = df_to_return.transpose()
-            s = pd.Series(main_df.iloc[j])
-            df_to_return = pd.concat([df_to_return, s.to_frame().T], ignore_index=True) 
-        j = j+1
-    #end_timer
-    end_timer = time.perf_counter()
-    print("find_item_in_DataFrame timer =", end_timer-start_timer)
-    return(df_to_return)
 
-def find_many_rows_in_DataFrame_without_concat (
-    main_df, #DataFrame that contain our object 
-    object_to_search_for, #what we need to find
-    item_column #name of column to look into for item
-    ):
-    """
-        Finds all enteties that we are looking for in given DataFrame
-    """
-    #start timer
-    start_timer = time.perf_counter()
-    #looking for the item
-    #dict_to_df = []
+    #declare a variable
     dict_to_df = {}
+
+    #we are looking for the item we need and adding it to the list of dictionaries 
     j = 0
     for i in main_df.loc[:,item_column]:
         if i == object_to_search_for:
-            #df_to_return=main_df.iloc[j]
-            #df_to_return = df_to_return.transpose()
-            s = pd.Series(main_df.iloc[j]).T
-            dict_to_df[len(dict_to_df)] = s.to_dict()
+            #dict_to_df[len(dict_to_df)] = pd.Series(main_df.iloc[j]).T.to_dict()
+            dict_to_df[len(dict_to_df)] = pd.Series(main_df.iloc[j]).T.to_dict()
         j = j+1
-    print (dict_to_df)
-    df_to_return = pd.DataFrame.from_records(dict_to_df)
-    #end_timer
+    
+    #transforming list of dicts into dataframe
+    df_to_return = pd.DataFrame.from_dict(dict_to_df, orient="index")
+    
+    #end_timer + return
     end_timer = time.perf_counter()
-    print("find_many_rows_in_DataFrame_without_concat timer =", end_timer-start_timer)
+    print(find_many_rows_in_DataFrame.__name__, " timer =", end_timer-start_timer)
     return(df_to_return)
 
 static_database = {
@@ -127,16 +105,29 @@ dynamic_database = {
 }
 
 
-column = ["raid id"]
-main_df = read_the_file_to_DF(dynamic_database["guilds_table"])
-lable = find_1_row_in_DataFrame(main_df,"GGuild", "guild_name")
-main_df = read_the_file_to_DF(dynamic_database["characters_table"])
-item = find_many_rows_in_DataFrame_without_concat(main_df,lable.loc["guild_id"],"guild_id")
-res = item.to_csv("123.csv")
-print(item,"\n")
+
 
 
 """
+column = ["raid id"]
+main_df = read_the_file_to_DF(
+    file_to_read = dynamic_database["guilds_table"])
+lable = find_1_row_in_DataFrame(
+    main_df,
+    object_to_search_for="GGuild",
+    item_column =  "guild_name"
+    )
+main_df = read_the_file_to_DF(
+    file_to_read = dynamic_database["characters_table"]
+    )
+item = find_many_rows_in_DataFrame(
+    main_df, 
+    object_to_search_for = lable.loc["guild_id"],
+    item_column = "guild_id"
+    )
+print(item,"\n")
+
+
 main_df = read_the_file_to_DF(file_to_look_into)
 #item = find_item_in_DataFrame(main_df,"raid_1", "raid id")
 print(main_df,"\n")
--- utils/useless_tools.py
+++ utils/useless_tools.py
@@ -19,3 +19,34 @@ def find_item_in_DataFrame_without_for (
     end_timer = time.perf_counter()
     print("find_item_in_DataFrame_without_for timer =", end_timer-start_timer)
     return(item)
+
+def find_many_rows_in_DataFrame_with_for__concat (
+    main_df, #DataFrame that contain our object 
+    object_to_search_for, #what we need to find
+    item_column #name of column to look into for item
+    ):
+    """
+        Finds all enteties that we are looking for in given DataFrame
+    """
+    #start timer
+    start_timer = time.perf_counter()
+
+    #declare a variable
+    df_to_return = pd.DataFrame(columns=main_df.columns)
+
+    #looking for the item
+    j = 0
+    for i in main_df.loc[:,item_column]:
+        if i == object_to_search_for:
+            #df_to_return=main_df.iloc[j]
+            #df_to_return = df_to_return.transpose()
+            s = pd.Series(main_df.iloc[j])
+            df_to_return = pd.concat([df_to_return, s.to_frame().T], ignore_index=True) 
+        j = j+1
+    
+    #end_timer
+    end_timer = time.perf_counter()
+    print(find_many_rows_in_DataFrame_with_for__concat.__name__, " timer =", end_timer-start_timer)
+    
+    return(df_to_return)
+
