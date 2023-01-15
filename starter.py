4b36326738623f4c9eb9bcf6a8a957a5cb591715
GawarWawar <gawarwawar@gmail.com>
2023-01-15 17:05:26 +0200

    added name to guilds end point

--- starter.py
+++ starter.py
@@ -5,7 +5,7 @@ import json
 from flask import Flask, request, Response, jsonify, json
 from markupsafe import escape
 
-from utils.tools import read_the_file_to_DF, find_item_in_DataFrame
+from utils.tools import read_columns_of_the_csv_to_DF, read_the_file_to_DF, find_item_in_DataFrame
 
 app = Flask(__name__)
 enctype="multipart/form-data"
@@ -17,11 +17,10 @@ static_database = {
 }
 
 dynamic_database = {
-    "guilds_table" : "Data/Dynamic_database/guild_table.csv",
+    "guilds_table" : "Data/Dynamic_database/guilds_table.csv",
     "characters_table" : "Data/Dynamic_database/characters_table.csv",
-    "killed_bosses_table" : "Data/Dynamic_database/bosses_that_were_killed_table.csv",
     "runs_table" : "Data/Dynamic_database/runs_of_the_guilds_table.csv",
-    "dropped_items_table" : "Data/Dynamic_database/dropped_items_table.csv"
+    "events_table" : "Data/Dynamic_database/events_table.csv"
 }
 
 
@@ -40,11 +39,14 @@ def list_of_books():
     #return json.dumps(books, indent=4)
     return jsonify(books)
 
-@app.route("/guildStats") #methods = ["GET"]
+@app.route("/guildStats/<name>") #methods = ["GET"]
 #get all data about the guild
-def give_all_aviable_guild_stats():
-    df_to_work_with = read_the_file_to_DF()
-    return()
+def give_all_aviable_guild_stats(name):
+    guld_name = escape(name)
+    df_to_work_with_guild = read_the_file_to_DF(dynamic_database["guilds_table"])
+    df_to_work_with_characters = read_the_file_to_DF(dynamic_database["characters_table"])
+    df_to_send = 1
+    return (guld_name)
 
 @app.route("/raids") #methods = ["GET"]
 #user wants to create new raid and we need to give all of the raid to him
@@ -53,6 +55,7 @@ def create_new_raid ():
     df_to_send = df_to_work_with.iloc[:,[0,1]]
     result = json.loads(df_to_send.to_json(orient="index"))
     return json.dumps(result, indent=2)
+
 @app.route("/characters") #methods = ["GET"]
 #giving all the characters in the certain guild
 def characters_of_the_guild ():
