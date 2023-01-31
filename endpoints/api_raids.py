4a62ef89d8b000249c13785c4aab3df82329a760
GawarWawar <gawarwawar@gmail.com>
2023-01-31 18:29:53 +0200

    /raidsis ready w/ good info form

--- endpoints/api_raids.py
+++ endpoints/api_raids.py
@@ -5,12 +5,16 @@ import json
 
 def give_info_about_all_raids (static_database):
     #read the table w/ info about raids
-    df_for = pd.read_csv(
+    df_for_raids = pd.read_csv(
         static_database["raid_table"]
     )
     
-    #get needed columns where the info stored
-    df_to_send = df_for.loc[:,["raid_id","raid_name","raid_type"]]
+    result = json.loads(df_for_raids.to_json(orient="records"))
+    #orient="records" gives us -> 
+        #info about every raid in form of the list
+            #each elem of list have:
+            #   "raid_id"
+            #   "raid_name"
+            #   "raid_type"
     
-    result = json.loads(df_to_send.to_json(orient="index"))
     return json.dumps(result, indent=2)
