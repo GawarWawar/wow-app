5dee8bbec46359897c8f64619cffaaed2d7a38ae
GawarWawar <gawarwawar@gmail.com>
2023-02-01 18:18:07 +0200

    raidRun/:id GET w/ needed form of info

--- endpoints/api_raidRun_g_id.py
+++ endpoints/api_raidRun_g_id.py
@@ -85,42 +85,115 @@ def edit_raid_run_m(
     #GET
     else:
         #find run by its id in runs table
-        df_to_return = u_tools.find_item_in_DataFrame_without_for(
+        run_info = u_tools.find_item_in_DataFrame_without_for(
             df_for_runs,
             raid_run_id,
             "run_id"
         )
-
-        #find all events in events table by the run id
-        df_to_return = pd.DataFrame.merge(
-            df_to_return,
-            df_for_events,
+        
+        #clearing variables that we wont use anymore
+        df_for_runs = None
+        raid_run_id = None
+        
+        #creating dictionary that we will send
+        dict_to_send = {
+            #adding already known info from run_info
+            "guild_id": int(run_info.iloc[0].at["guild_id"]),
+            "raid_id": int(run_info.iloc[0].at["raid_id"]),
+            #creating structure
+            "run_members":[],
+            "bosses":[]
+        }
+        
+        #reading tables w/ info
+        df_characters = pd.read_csv(dynamic_database["characters_table"])
+        df_for_items = pd.read_csv(static_database["item_table"])
+        df_run_members = pd.read_csv(dynamic_database["run_members"])
+        df_boos_table = pd.read_csv(static_database["boss_table"])
+        
+        #getting info about only this run members
+        df_run_members = pd.DataFrame.merge(
+            df_run_members,
+            run_info,
             on="run_id"
         )
         
-        #get info about characters
-        df_for_characters = pd.read_csv(dynamic_database["characters_table"])
+        #deliting info about guild_id that come from run_info
+        df_run_members.pop("guild_id")
         
-        #find all characters, that were involved in run
-        df_to_return = pd.DataFrame.merge(
-            df_to_return,
-            df_for_characters,
-            on="character_id",
-            #there is guild in run table and characters table ->
-            suffixes=["_run","_character"] 
-        #we are giving them different names
+        #getting details about all characters in this run
+        df_run_members = pd.DataFrame.merge(
+            df_run_members,
+            df_characters,
+            on="character_id"
+        )
+
+                
+        for row in range(len(df_run_members.loc[:,"character_id"])):
+            #gathering info about run member
+            add_character =  {
+                "character_id": int(df_run_members.iloc[row].at["character_id"]),
+                "character_name": df_run_members.iloc[row].at["character_name"],
+                "character_class": df_run_members.iloc[row].at["class"]
+            }
+            
+            #adding info about run member into dict_to_send
+            dict_to_send["run_members"].append(add_character)
+        
+        #getting events only for our run
+        df_for_events = pd.DataFrame.merge(
+            df_for_events,
+            run_info,
+            on="run_id"
         )
         
-        #read info about items
-        df_for_items = pd.read_csv(static_database["item_table"])
-        
-        #find all items, that has been dropped 
-        df_to_return = pd.DataFrame.merge(
-            df_to_return,
+        #getting info about every looted item in this run
+        df_for_events = pd.DataFrame.merge(
+            df_for_events,
             df_for_items,
             on="item_id"
         )
         
-        #return json w/ full info
-        result = json.loads(df_to_return.to_json(orient="index"))
-        return json.dumps(result, indent=2)
+        #creating df about killed bosses
+        df_bosses = df_for_events["boss_id"]
+        #making bosses ids unique
+        df_bosses = df_bosses.drop_duplicates()
+        
+        #getting info about bosses, that were killed in this run
+        df_bosses = pd.DataFrame.merge(
+            df_bosses,
+            df_boos_table,
+            on="boss_id"
+        )
+        
+        #gather info about boss -> add to dict_to_send
+        for boss in df_bosses.loc[:,"boss_id"]:
+            add_boss = {
+                #adding info that we already know
+                "boss_id": int(df_bosses.iloc[boss].at["boss_id"]),
+                "boss_name": df_bosses.iloc[boss].at["boss_name"],
+                #forming structure for the loot
+                "dropped_loot":[]
+            }
+            
+            #getting loot for the boss, that we are adding r/n
+            boss_loot = df_for_events[df_for_events.loc[:, "boss_id"] == boss]
+            boss_loot = boss_loot.reset_index()
+            
+            #gather info about loot -> add to add_boss
+            for loot in range(len(boss_loot.loc[:,"boss_id"])):
+                #getting info about every loot
+                add_loot = {
+                    "item_id": int(boss_loot.iloc[loot].at["item_id"]),
+                    "item_name": boss_loot.iloc[loot].at["item_name"],
+                    "character_id": int(boss_loot.iloc[loot].at["character_id"])
+                }
+                
+                #adding loot to the add_boss
+                add_boss["dropped_loot"].append(add_loot)
+            
+            #adding bosses to the dict_to_send
+            dict_to_send["bosses"].append(add_boss)
+
+        #sendind gathered info about run
+        return json.dumps(dict_to_send, indent=2)
