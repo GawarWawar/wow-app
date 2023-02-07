9d6260c68261352eb44003fe66a18cf873665425
GawarWawar <gawarwawar@gmail.com>
2023-02-07 14:28:16 +0200

    added add_row to tools.py needed to add sys path feature to it too

--- utils/tools.py
+++ utils/tools.py
@@ -4,6 +4,14 @@ import json
 
 import time
 
+from os.path import dirname, abspath
+import sys
+
+SCRIPT_DIR = dirname(abspath(__file__))
+sys.path.append(dirname(SCRIPT_DIR))
+
+import utils.add_row as add_row
+
 
 #change separetor from ", " to ","
 def rewrite_wowhead_separator(
@@ -178,3 +186,37 @@ def extend_list_by_dict_from_df (
     list_to_extend.extend(json.loads(df_to_add))
     
     return(None)
+
+def check_character_existence_add_if_not (
+    df_characters, #df for all characters (characters table)
+    #members of run we need to check if they exist in df_characters
+    df_run_members, 
+    run_member_counter, #inside counter for the members
+    #if we need to create new member, give him this id
+    new_members_guild_id 
+):
+    #find character in the character table
+    member_existence = \
+        find_item_in_DataFrame_without_for(
+            df_characters,
+            df_run_members.loc[run_member_counter,"name"],
+            "character_name"
+        )
+        
+    #check if this character already exist 
+    if member_existence.empty:
+        #adding new character to the character_table
+        character_id = add_row.id_and_three_columns(
+            df_characters,
+            dict_w_info={
+                #info about that character we need to write
+                0:df_run_members.loc[run_member_counter,"name"],
+                1:new_members_guild_id,
+                2:df_run_members.loc[run_member_counter,"character_class"],
+            }
+        )
+    else:
+        #if exist -> getting its id
+        character_id = member_existence.iloc[0].at["character_id"] 
+    
+    return(character_id)
