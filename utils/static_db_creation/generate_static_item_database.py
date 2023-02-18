86e0fef6a98499567844ce58f24119dd2f966956
GawarWawar <gawarwawar@gmail.com>
2023-02-18 02:49:32 +0200

    polish generate_static_item_database.py

--- utils/static_db_creation/generate_static_item_database.py
+++ utils/static_db_creation/generate_static_item_database.py
@@ -2,20 +2,12 @@ import numpy as np
 import pandas as pd
 import time
 
-from os import listdir
-from os.path import isfile, join, dirname, abspath
-#
-#import sys
-#
-#SCRIPT_DIR = dirname(abspath(__file__))
-#sys.path.append(dirname(SCRIPT_DIR))
-
-import db_creation_utils.db_creation_tools as db_tools
-
 start_timer = time.perf_counter()
 
-df = pd.read_csv(
-    "data/data_for_staic_db/manually_changed_static_db/Wow app - Transfer sheet for Items.csv",
+
+# Reading table w/ info about loot in Naxx
+Naxx_df = pd.read_csv(
+    "data/data_for_staic_db/manually_changed_static_db/Wow app - Items_from_Naxx_transfer.csv",
     usecols=[
         "boss_name",
         "item_id",
@@ -24,7 +16,7 @@ df = pd.read_csv(
     index_col=[0]
 )
 
-#building table for loot that drops from bosses
+# Read names and ids of all bosses
 bosses_list = pd.read_csv(
     "data/data_for_staic_db/manually_changed_static_db/Wow app - Bosses table.csv",
     usecols=[
@@ -33,53 +25,60 @@ bosses_list = pd.read_csv(
     ]
 )
 
-drop_dict = {
+# Dict for loot_table
+loot_dict = {
     "boss_id" : [],
     "item_id" : []
 }
-all_items_ids = []
-all_items_names = []
-for df_index in df.index:
-    item_ids = str(df.loc[df_index]["item_id"]).split(", ")
+# List of all items in the raids
+all_items_ids = [] #theirs ids
+all_items_names = [] #theirs names
+
+wowhead_separator = ", "
+for df_index in Naxx_df.index:
+    # Getting items for the first boss
+    item_ids = str(Naxx_df.loc[df_index]["item_id"]).split(wowhead_separator)
     
     boss_id = bosses_list.loc[bosses_list.loc[:,"boss_name"]==df_index]
     boss_id = boss_id.iloc[0].at["boss_id"]
     
-    #creat DataFrame w/ drop for certain boss
-    df_add_drop = pd.DataFrame(
+    # Creat DataFrame w/ loot for certain boss
+    df_add_loot = pd.DataFrame(
         {
             "item_id": item_ids,
             "boss_id": boss_id
         }
     )
     
-    boss_dict = df_add_drop.to_dict(orient="list")
-    #create dictiorary to transform into df
-    drop_dict["boss_id"].extend(boss_dict["boss_id"])
-    drop_dict["item_id"].extend(boss_dict["item_id"])
-
+    boss_dict = df_add_loot.to_dict(orient="list")
+    
+    # Add loot from boss to loot_dict
+    loot_dict["boss_id"].extend(boss_dict["boss_id"])
+    loot_dict["item_id"].extend(boss_dict["item_id"])
+    
     all_items_ids.extend(
         item_ids
     )
     all_items_names.extend(
-        str(df.loc[df_index]["item_name"]).split(", ")
+        str(Naxx_df.loc[df_index]["item_name"]).split(wowhead_separator)
     )
 
 path_to_st_db="Data/Static_database"
 
-drop_df = pd.DataFrame.from_dict(drop_dict) 
-drop_df = drop_df.sort_values(
+# Writing prosses into loot_table
+loot_df = pd.DataFrame.from_dict(loot_dict) 
+loot_df = loot_df.sort_values(
     by=["boss_id","item_id"], 
     ignore_index=True
 )   
-
-drop_df.to_csv(
+loot_df.to_csv(
         path_to_st_db+"/"+"loot_of_bosses.csv", 
         index_label=False, 
         index=False,
         header=True, 
     )
 
+# Writing prosses into items_table
 df_items = pd.DataFrame(
     {
         "item_id": all_items_ids,
@@ -96,6 +95,7 @@ df_items.to_csv(
         index=False,
         header=True, 
     )
+
 
 end_timer = time.perf_counter()
 print(
